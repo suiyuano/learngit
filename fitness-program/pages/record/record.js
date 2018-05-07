f43b685f0f9e35092b6f2d4f3357d68438dfa03a
suiyuan <1095529505@qq.com>
2018-05-07 21:33:57 +0800

    add a new page named record

---
+++ fitness-program/pages/record/record.js
@@ -0,0 +1,73 @@
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    first_section:'深蹲',
+    second_section:'箭步蹲',
+    third_section:'腿举',
+    fourth_section:'腿屈伸',
+    fifth_section:'腿弯举',
+    sixth_section:'提踵',
+    seventh_section:'相扑硬拉',
+    eighth_section:'臀推',
+    ninth_section:'直腿硬拉',
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+    
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+    
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+    
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+    
+  }
+})
