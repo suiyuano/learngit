599c27e9e36656b989ba81f97af08902be930649
suiyuan <1095529505@qq.com>
2018-05-31 12:19:46 +0800

    complished rhe function share

--- food-program/pages/index/index.js
+++ food-program/pages/index/index.js
@@ -50,5 +50,23 @@ Page({
       userInfo: e.detail.userInfo,
       hasUserInfo: true
     })
+  },
+  onShareAppMessage: function (res) {
+    if (res.from === 'button') {
+      // 来自页面内转发按钮
+      console.log(res.target)
+    }
+    return {
+      title: '我的美食报告',
+      path: '/pages/report/report'
+    }
+    wx.showShareMenu({
+      withShareTicket: true
+    })
+    wx.updateShareMenu({
+      withShareTicket: true,
+      success() {
+      }
+    })
   }
 })
